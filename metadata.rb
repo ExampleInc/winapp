8aa6eed257033b6237ab77cdbccb51e1816b8278
Dmitrii Mostovshchikov <Dimitry.Mostovshikov@Li9.com>
2016-07-21 00:43:48 -0500

    Updated css

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures winapp'
 long_description 'Installs/Configures winapp'
-version '0.1.38'
+version '0.1.39'
 
 depends 'sql_server', '~> 2.5.0'
 depends 'iis', '~> 4.1.6'
